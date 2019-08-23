a6c7551d6bd216e2736d4228923b3ac1a9976006
Michel G. Souza <michels@alunos.utfpr.edu.br>
2019-08-23 01:31:51 -0300

    Adicionando o core.
    Feito um split da frase, e adicionado um switch case

--- code/app/src/main/Main.kt
+++ code/app/src/main/Main.kt
@@ -1,2 +1,28 @@
 package main
 
+
+fun main(args: Array<String>) {
+
+var cmd : String = "";
+
+while(cmd !=  "exit" ){
+
+		print("> ");
+		cmd = readLine()!!;
+		var cmd_parts = cmd.split(" ");
+
+		when(cmd_parts[0]){
+			"cd" -> cd_command(cmd_parts);
+			"ls" -> ls_command(cmd_parts);
+		}
+
+	}
+}
+
+fun  cd_command(cmd_parts: List<String>){
+	println("Você digitou CD, o restante dos parametros é $cmd_parts ");
+}
+
+fun  ls_command(cmd_parts: List<String>){
+	println("Você digitou LS, o restante dos parametros é $cmd_parts ");
+}
