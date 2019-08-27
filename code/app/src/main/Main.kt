34bc5e3134aba1b2ae721443447f46de56637524
Michel Gomes de Souza <michels@alunos.utfpr.edu.br>
2019-08-26 21:00:13 -0300

    Adicionado suporte para cores

--- code/app/src/main/Main.kt
+++ code/app/src/main/Main.kt
@@ -1,30 +1,73 @@
 package main
 import java.io.File
 //http://thetechnocafe.com/build-a-file-explorer-in-kotlin-part-5-creating-deleting-files-and-folders/
+//
+/*
+kotlinc hello.kt -include-runtime -d hello.jar
+
+java -jar hello.jar
+
+ */
 //Referencias
+
+/* Sistema de Cores */
+const val ESC = "\u001B"
+const val NORMAL = ESC + "[0"
+const val BOLD   = ESC + "[1"
+const val BLACK_B  = ESC + "[0;40m"  // black background
+const val WHITE_F  = ESC + "[0;37m"  // normal white foreground
+
+const val RED = ";31m"
+const val GREEN = ";32m"
+const val YELLOW = ";33m"
+const val BLUE = ";34m"
+const val MAGENTA = ";35m"
+const val CYAN = ";36m"
+const val WHITE = ";37m"
+const val TESTE = ";92m"
+
 
 var currentFolder = ArrayList<String>()
 
-
 fun main(args: Array<String>) {
-currentFolder.add("./")
-var cmd : String = "";
+	logo_command()
+	currentFolder.add("./")
+	var cmd : String = "";
 
-while(cmd !=  "exit" ){
-		print(stackFolder());
-		print(" > ");
-		cmd = readLine()!!;
-		var cmd_parts = cmd.split(" ");
-		when(cmd_parts[0]){
-			"cd" -> cd_command(cmd_parts);
-			"ls" -> ls_command();
-			"mkdir" -> mkdir_command(cmd_parts);
-			"rmdir" -> rmdir_command(cmd_parts);
-			"man" -> man_command(cmd_parts);
-			else -> println("Esse comando não existe, tente 'man help'.")
-		}
+	while(cmd !=  "exit" ){
+			print(stackFolder());
+			print(" > ");
+			cmd = readLine()!!;
+			var cmd_parts = cmd.split(" ")
+			when(cmd_parts[0]){
+				"cd" -> cd_command(cmd_parts)
+				"ls" -> ls_command()
+				"mkdir" -> mkdir_command(cmd_parts)
+				"rmdir" -> rmdir_command(cmd_parts)
+				"clear" -> print("${ESC}c")
+				"man" -> man_command(cmd_parts)
+				"exit" -> println("Saindo do Shell!")
+				else -> println("Esse comando não existe, tente 'man help'.")
+			}
 
 	}
+}
+
+fun logo_command(): String{
+	print("${ESC}c") //Limpa a Tela
+	println("\n$NORMAL;92m    88      a8P                    88 88              ad88888ba  88                     88 88 $WHITE_F")  
+ 	println("$NORMAL;93m    88    ,88'               ,d    88 ''             d8'     '8b 88                     88 88 $WHITE_F")  
+	println("$NORMAL;94m    88  ,88'                 88    88                Y8,         88                     88 88 $WHITE_F")  
+ 	println("$NORMAL;95m    88,d88'      ,adPPYba, MM88MMM 88 88 8b,dPPYba,  `Y8aaaaa,   88,dPPYba,   ,adPPYba, 88 88 $WHITE_F")  
+ 	println("$NORMAL;96m    888888,    a8''     '8a  88    88 88 88P'   ''8a   `'''''8b, 88P'    '8a a8P_____88 88 88 $WHITE_F")  
+ 	println("$NORMAL;97m    88P   Y8b   8b       d8  88    88 88 88       88         `8b 88       88 8PP''''''' 88 88 $WHITE_F")
+ 	println("$NORMAL;98m    88     '88, '8a,   ,a8'  88,   88 88 88       88 Y8a     a8P 88       88 '8b,   ,aa 88 88 $WHITE_F")  
+ 	println("$NORMAL;99m    88       Y8b `'YbbdP''   'Y888 88 88 88       88  'Y88888P'  88       88  `'Ybbd8'' 88 88 $WHITE_F\n\n")
+
+	println("Versão 0.1\n\n")
+
+	return ""
+
 }
 
 fun man_command(cmd_parts: List<String>){
@@ -34,9 +77,10 @@ fun man_command(cmd_parts: List<String>){
 			"ls" -> println("\nLS: comando para mostrar o conteudo da pasta atual.\n")
 			"rmdir" -> println("\nRMDIR: comando para excluir pastas.\nEx: rmdir teste.\n")
 			"mkdir" -> println("\nMKDIR: comando para criar pastas.\nEx: mkdir teste.\n")
-			"exit" -> println("\nExit: comando para sair do shell.\n")
-			"help" -> println("\nComandos Disponiveis: cd, ls, rmdir, mkdir, help, man, exit.\nPara saber mais informações é só digitar 'man <comando>'.\nEx: man ls\n")
-			"man" -> println("MAN: comando do manual, digite 'man help' para obter mais informações.")
+			"exit" -> println("\nEXIT: comando para sair do shell.\n")
+			"clear" -> println("\nCLEAR: comando para limpar a tela.\n")
+			"help" -> println(logo_command()+"\nComandos Disponiveis: cd, ls, rmdir, mkdir, help, man, exit.\nPara saber mais informações é só digitar 'man <comando>'.\nEx: man ls\n")
+			"man" -> println("MAN: comando do manual, digite 'man help' para obter mais informações.");
 			else -> println("Esse comando não existe, tente 'man help'.")	
 		}
 	}
@@ -63,7 +107,12 @@ fun  cd_command(cmd_parts: List<String>){
 fun  ls_command(){
 	val folders = File(stackFolder()).listFiles().map{ it.name }
 	for (name in folders){
-		print("$name ")
+		if(File(name).isDirectory()){
+			print("$NORMAL$CYAN$name $WHITE_F")
+		}else{
+			print("$name ")
+		}
+
 	}
 	println("")
 }
